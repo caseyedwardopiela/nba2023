dcb0935294254ec0c94ce492b73f1aa4227f9060
caseyedwardopiela <96747682+caseyedwardopiela@users.noreply.github.com>
2022-09-18 20:56:51 -0700

    Create 2k23-py.py

---
+++ 2k23-py.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('v1_2023_predictions.csv').drop(['Unnamed: 0'], axis = 1)
+
+st.title('2022-2023 NBA Fantasy Score Predictions')
+
+st.markdown("""
+This app pulls historical data from Basketball-Reference.com, including college, international, and regular season stats. After compiling everything,
+it runs a combination of several machine learning algorithms to predict all of the traditional stats for each player this coming season.
+""")
+
+st.sidebar.header('User Input Features')
+
+# Sidebar - Team Selection
+sorted_team_unique = sorted( df['Player_Team'].unique() )
+selected_team = st.sidebar.multiselect('Team', sorted_team_unique, sorted_team_unique)
+
+# Filtering data
+df_selected_team = df[ (df['Player_Team'].isin(selected_team))]
+
+st.header('Display Players on Selected Team')
+st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
+st.dataframe(df_selected_team)
