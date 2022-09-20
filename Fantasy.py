2742fbbdc4f1d15487f8586d59e658190d558839
caseyedwardopiela <96747682+caseyedwardopiela@users.noreply.github.com>
2022-09-20 14:24:04 -0700

    Update Fantasy.py

--- Fantasy.py
+++ Fantasy.py
@@ -20,7 +20,7 @@ st.sidebar.header('User Input Selection')
 # Sidebar - Team Selection
 sorted_team_unique = list(df['Player_Team'].unique())
 sorted_team_unique = sorted(sorted_team_unique)
-sorted_position_unique.insert(0,'All Teams')
+sorted_team_unique.insert(0,'All Teams')
 selected_team = st.sidebar.selectbox('Which team would you like to see?',
     sorted_team_unique)
 
